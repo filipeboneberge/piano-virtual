05bf83de5524d3b56efebb963bd57b2e466fcc70
filipeboneberge <filipeboneberge@hotmail.com>
2026-02-15 15:50:33 -0300

    keydown-teclas-permitidas

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,11 +1,25 @@
 const pianoKeys = document.querySelectorAll(".piano-keys .key");
+const mapedKeys = [];
 
 const playTune = (key) => {
   let audio = new Audio(`src/tunes/${key}.mp3`);
   audio.play();
+
+  const clikedKey = document.querySelector(`[data-key="${key}" ]`);
+  clikedKey.classList.add("active");
+  (setTimeout(() => {
+    clikedKey.classList.remove("active");
+  }),
+    150);
 };
 
 pianoKeys.forEach((key) => {
-  console.log(key.dataset.key);
   key.addEventListener("click", () => playTune(key.dataset.key));
+  mapedKeys.push(key.dataset.key);
+});
+
+document.addEventListener("keydown", (e) => {
+  if (mapedKeys.includes(e.key)) {
+    playTune(e.key);
+  }
 });
